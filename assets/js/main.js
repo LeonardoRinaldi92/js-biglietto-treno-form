dabd078a27e66fa4826a8dac0b618e11452953f2
LeonardoRinaldi92 <leonardo.rinaldi.ph@gmail.com>
2023-03-23 15:56:43 +0100

    struttura 3

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 let partenza = '';
 let arrivo = '';
+let sameCity = 'La città di partenza e quella di arrivo non possono combaciare!'
 
 document.getElementById('partenza').addEventListener("change", (event) => {
     partenza = event.target.value;
@@ -9,7 +10,17 @@ document.getElementById('arrivo').addEventListener("change", (event) => {
     arrivo = event.target.value;
 });
 
+const form = document.getElementById("form");
 
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    if (partenza === arrivo)
+        alert (sameCity)
+
+    }
+
+)
 
 
 
