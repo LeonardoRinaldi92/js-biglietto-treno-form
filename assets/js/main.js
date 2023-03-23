1ef9e4da6b9c0b8f2249c780e9190d50e7bc0cd4
LeonardoRinaldi92 <leonardo.rinaldi.ph@gmail.com>
2023-03-23 15:42:25 +0100

    struttra 2

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+let partenza = '';
+let arrivo = '';
+
+document.getElementById('partenza').addEventListener("change", (event) => {
+    partenza = event.target.value;
+});
+
+document.getElementById('arrivo').addEventListener("change", (event) => {
+    arrivo = event.target.value;
+});
+
+
+
+
+
+
+
+
